07ccf9b64d28873017f40219b5c9402ef8e3906a
Hecze <hectorzerrillo85@gmail.com>
2024-07-08 23:45:09 -0500

    update eneisoft icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
         <NavbarBrand>
           <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse sm:hidden md:flex">
             <img
-              src="https://scontent.flim15-1.fna.fbcdn.net/v/t39.30808-1/363413231_1150809719203883_3289886993161493055_n.jpg?stp=dst-jpg_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=f4b9fd&_nc_eui2=AeEFkQIep-u5IEIrP6aXv5y1t2GAWmuC3eG3YYBaa4Ld4XBoJmvvZBQn1dDo45PKI9EqQRxsd-EUong-eHI6w2bA&_nc_ohc=byv1bKluDkQQ7kNvgE6zAsU&_nc_ht=scontent.flim15-1.fna&gid=A_R93mTEpbi5oNOdMQATbEb&oh=00_AYCAk5peko4I-Q6Edq-nCUjDxOrjoLcrRNOWhTTLUj6rQw&oe=668BA3F7"
+              src="/images/favicon.ico"
               className="h-8"
               alt="ENEISOFT Logo"
             />
