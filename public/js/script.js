ec06703fc7393091835d744ed12c40662f4462f8
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-10-31 02:30:39 -0500

    sponsors y aliados divs

--- public/js/script.js
+++ public/js/script.js
@@ -115,8 +115,8 @@ function fillAliados(aliados_sponsors) {
     aliados_sponsors.forEach(element => {
         if (element.visible) {
             $(`#${element.type}s-container`).append(`<div class="col-sm-12 col-md-6 col-lg-4 p-2 py-md-5 px-md-2">
-                <div class="text-center px-2 py-4 bg-white">
-                    <h5 class="text-white d-flex justify-content-center align-items-center gap-2 fs-4 fw-bold text-nowrap">
+                <div class="text-center px-2 py-4 bg-white h-100 d-flex flex-column">
+                    <h5 class="text-white d-flex justify-content-center align-items-center gap-2 fs-4 fw-bold text-nowrap flex-grow-1">
                         <img src="./img/${element.type}s/${element.nombre.toLowerCase()}.png" height=250/>
                     </h5>
                     <p class="text-negro py-2 mx-1 mx-lg-5 text-center" style="font-size: 18px;">
